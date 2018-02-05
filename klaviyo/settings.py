d5d0b746065ed2560a5812b35d16eff8752899b4
jl781 <jl781@duke.edu>
2018-02-05 05:28:26 -0500

    change db

--- klaviyo/settings.py
+++ klaviyo/settings.py
@@ -91,34 +91,34 @@ WSGI_APPLICATION = 'klaviyo.wsgi.application'
 
 
 '''
-NOTICE: REMEMBER TO SWITCH DBs WHEN USING DIFFERENT SERVERS. 
+NOTICE: REMEMBER TO SWITCH DBs WHEN USING DIFFERENT SERVERS.
 We will choose to use mysql instead of sqlite3 b/c mysql supports multi-thread situation
 '''
 # Local MySQL db
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'klaviyo',
-        'USER': 'root',
-        'PASSWORD': 'fantasy',
-        'HOST': 'localhost',
-        'PORT': ''
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'klaviyo',
+#         'USER': 'root',
+#         'PASSWORD': 'fantasy',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
 
 # Heroku remote host db
 # mysql://b51dc2bd6e1c12:ef47f796@us-cdbr-iron-east-05.cleardb.net/heroku_112c84ebeeb128b?reconnect=true
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'heroku_112c84ebeeb128b',
-#         'USER': 'b51dc2bd6e1c12',
-#         'PASSWORD': 'ef47f796',
-#         'HOST': 'us-cdbr-iron-east-05.cleardb.net',
-#         'PORT': ''
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroku_112c84ebeeb128b',
+        'USER': 'b51dc2bd6e1c12',
+        'PASSWORD': 'ef47f796',
+        'HOST': 'us-cdbr-iron-east-05.cleardb.net',
+        'PORT': ''
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
